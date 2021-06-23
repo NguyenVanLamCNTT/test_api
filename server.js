97beccb791a2ae13f7ea03e25e4f5b8354ae7ba6
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2021-06-23 19:59:46 +0700

    fix cors

--- server.js
+++ server.js
@@ -21,3 +21,12 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
+const cors = require('cors');
+
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
